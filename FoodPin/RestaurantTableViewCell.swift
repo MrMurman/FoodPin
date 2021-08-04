61fafe4b2f1bb9b7ff8d71822ddb4a02adfa4d30
MrMurman <74971676+MrMurman@users.noreply.github.com>
2021-08-04 22:21:48 +0300

    Chapter 10 - before iPad

--- FoodPin/RestaurantTableViewCell.swift
+++ FoodPin/RestaurantTableViewCell.swift
@@ -18,7 +18,8 @@ class RestaurantTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.tintColor = .systemYellow
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -14,9 +14,11 @@ class RestaurantTableViewController: UITableViewController {
         var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
         var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
         var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+        
+        var restaurantIsFavorite = Array(repeating: false, count: 21)
     }
     
-    let restaurant = Restaurant()
+    var restaurant = Restaurant()
     
     lazy var dataSource = configureDataSource()
     
@@ -50,6 +52,7 @@ class RestaurantTableViewController: UITableViewController {
             cell.thumbnailImageView.image = UIImage(named: self.restaurant.restaurantImages[indexPath.row])
             cell.locationLabel.text = self.restaurant.restaurantLocations[indexPath.row]
             cell.typeLabel.text = self.restaurant.restaurantTypes[indexPath.row]
+            cell.accessoryType = self.restaurant.restaurantIsFavorite[indexPath.row] ? .checkmark : .none
             
             return cell
         }
@@ -57,6 +60,41 @@ class RestaurantTableViewController: UITableViewController {
         return dataSource
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        
+        // Create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        // add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        // add "reserve a table" action
+        let reserveActionHandler = {(action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Not available yet", message: "Sorry, this feature is not available yet. Please retry later", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        
+        let reserveAction = UIAlertAction(title: "Reserve a table", style: .default, handler: reserveActionHandler)
+        
+        
+        // mark as favourite action
+        let favoriteAction = UIAlertAction(title: "Mark as favourite", style: .default) { (action: UIAlertAction) -> Void in
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+            cell?.tintColor = .systemYellow
+            self.restaurant.restaurantIsFavorite[indexPath.row] = true
+        }
+        
+        optionMenu.addAction(cancelAction)
+        optionMenu.addAction(reserveAction)
+        optionMenu.addAction(favoriteAction)
+        
+        present(optionMenu, animated: true, completion: nil)
+        
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
     
     
 }
